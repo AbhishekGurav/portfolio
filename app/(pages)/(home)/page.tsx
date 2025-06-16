6d681076e058f8c0269e828022cac0ad68893536
Abhishek Gurav <abhishekgurav97@gmail.com>
2025-06-16 22:19:21 +0530

    test

--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="w-[7%] min-w-36 subpixel-antialiased flex flex-col justify-end">
         <TypographySmall>
-        Born in 1997, based in Mumbai, I've always been passionate about technology and what it can provide to this world we live in.
+        Born in 1997, Mumbai, I've always been passionate about technology and what it can provide to this world we live in.
         Although my technical expertise and experience are quite frontend heavy. I'm hopeful to become an AI Engineer in the future.
         </TypographySmall>
     </div>
