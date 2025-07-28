86253a0fff36ec30db34e5f8d76db44c68032351
Abhishek Gurav <abhishekgurav97@gmail.com>
2025-07-28 22:15:43 +0530

    three.js background added

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { NavSection } from "@/components/NavSection";
 import { NameSection } from "@/components/NameSection";
 import { ThemeControls } from "@/components/ThemeControls";
 import { ThemeProvider } from "@/context/ThemeContext";
+import { Background } from "@/components/Background";
 
 
 
@@ -16,10 +17,11 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${inter.variable} ${sharetechmono.variable} h-full w-full subpixel-antialiased`}>
-        <body className="font-family h-full w-full antialiased p-12 max-sm:p-2">
+        <body className="font-family h-full w-full antialiased p-12 max-sm:p-2 relative">
           <ThemeProvider>
+            <Background />
             <div className="h-full w-full p-8 border-1 border-foreground flex flex-row min-sm:justify-between 
-            max-sm:flex-col max-sm:p-2 
+            max-sm:flex-col max-sm:p-2 relative backdrop-blur-[6px]
             ">
               <div className=" flex flex-col max-sm:h-fit">
                 <NameSection />
