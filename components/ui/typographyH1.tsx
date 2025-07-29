db87b328e41fafafc45f8b3bf0999d96060e9545
Abhishek Gurav <abhishekgurav97@gmail.com>
2025-07-29 20:27:37 +0530

    font weight change in open source

--- components/ui/typographyH1.tsx
+++ components/ui/typographyH1.tsx
@@ -7,7 +7,7 @@ export function TypographyH1({children}: Readonly<{children : ReactNode}>) {
   
   return (
     <h1 className={cn(
-      "scroll-m-20 text-center text-4xl font-medium font-family tracking-tight text-balance")}>
+      "scroll-m-20 text-center text-4xl font-light font-family tracking-tight text-balance")}>
       {children}
     </h1>
   )
